c9fcadcdeccc8aac92c0f82d45ec50ca19585182
Einar Leite Austnes <einar.leite.austnes@km.kongsberg.com>
2024-08-07 17:04:10 +0200

    changed to debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -40,6 +40,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'cpptools',
       },
     }
 
@@ -96,38 +97,29 @@ return {
       },
     }
 
-    dap.adapters.cppdbg = {
-      id = 'cppdbg',
-      type = 'executable',
-      command = '$HOME/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
-    }
-
-    dap.configurations.cpp = {
-      {
-        name = "Launch file",
-        type = "cppdbg",
-        request = "launch",
-        --        program = function()
-        --          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build/', 'caf.out')
-        --        end,
-        cwd = '${workspaceFolder}',
-        stopAtEntry = true,
-      },
-      {
-        name = 'Attach to gdbserver :1234',
-        type = 'cppdbg',
-        request = 'launch',
-        MIMode = 'gdb',
-        miDebuggerServerAddress = 'localhost:1234',
-        miDebuggerPath = '/usr/bin/gdb',
-        cwd = '${workspaceFolder}',
-        --        program = function()
-        --         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build/', 'caf.out')
-        --        end,
-      },
-
-      -- Install golang specific config
-      require('dap-go').setup()
-    }
+ dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
   end,
 }
